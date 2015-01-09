1931befe9c8af12667e4452c586dde73b494349a
anthonyray <dantho361@hotmail.com>
2015-01-09 17:24:19 +0100

    Adding website to the repository

--- website/js/test.js
+++
@@ -1,8 +0,0 @@
-var neural = require('./network');
-var Layer = require('./layer');
-
-net = new neural.Network();
-
-net.layers.push(new Layer(3,50));
-net.layers.push(new Layer(3,50));
-net.layers.push(new Layer(3,50));
